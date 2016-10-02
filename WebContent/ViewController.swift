e13477182ae2ce7ad9784e125660a4e79ce37a64
Tim Myers <timmyers1978@gmail.com>
2016-10-01 22:44:27 -0600

    initial commit

--- WebContent/ViewController.swift
+++ WebContent/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://denvercoders.com")!
+        
+        webView.loadRequest(URLRequest(url: url))
+        
     }
 
     override func didReceiveMemoryWarning() {
