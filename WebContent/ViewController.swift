2ff796d6d695a1dd4e94acd1c75c63c565ceb863
Tim Myers <timmyers1978@gmail.com>
2016-10-01 23:06:40 -0600

    done

--- WebContent/ViewController.swift
+++ WebContent/ViewController.swift
@@ -16,9 +16,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let url = URL(string: "https://denvercoders.com")!
+        let url = URL(string: "https://stackoverflow.com")!
         
-        webView.loadRequest(URLRequest(url: url))
+        //webView.loadRequest(URLRequest(url: url))
+        
+        //webView.loadHTMLString("<h1>Hello there!</h1>", baseURL: nil)
+        
+        let request = NSMutableURLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print(error)
+            } else {
+                if let unwrappedData = data {
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    print(dataString)
+                }
+            }
+            
+        }
+        task.resume()
         
     }
 
